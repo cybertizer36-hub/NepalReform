a05bbfe6d3dfd81644db6fcb9358b12bb68d2ae5
bibeksah-dev <bibekanchansah@gmail.com>
2025-09-18 17:41:05 +0545

    Update testimonial-carousel.tsx

--- components/testimonial-carousel.tsx
+++ components/testimonial-carousel.tsx
@@ -138,12 +138,12 @@ function TestimonialCard({ testimonial }: { testimonial: Testimonial }) {
                 aria-label={`${testimonial.name}'s LinkedIn profile`}
               >
                 <svg
-                  className="w-3 h-3 text-white"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
-                </svg>
+                    className="w-3 h-3 text-white"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M10.59 13.41a1 1 0 0 1 1.41 0l4.59 4.59a3 3 0 0 0 4.24-4.24l-3.54-3.54a1 1 0 1 1 1.42-1.42l3.54 3.54a5 5 0 0 1-7.07 7.07l-4.59-4.59a1 1 0 0 1 0-1.41zM13.41 10.59a1 1 0 0 1-1.41 0L7.41 6a3 3 0 1 0-4.24 4.24l3.54 3.54a1 1 0 0 1-1.42 1.42L1.29 11.7a5 5 0 0 1 7.07-7.07l4.59 4.59a1 1 0 0 1 0 1.41z"/>
+                  </svg>
               </a>
             )}
           </div>
